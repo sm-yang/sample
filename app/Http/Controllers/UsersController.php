506ebebcf657bad52778ae8496cb10a2a8b93694
Summy <2437404258@qq.com>
2018-06-21 12:44:33 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function create () {
         return view('users.create');
+    }
+
+
+
+    public function show (User $user) {
+        return view('users.show', compact('user'));
+    }
+
+
+
+    public function store (Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        $request['password'] = bcrypt($request['password']);
+        $user = User::create($request->all());
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
