5eaf85117ecb4349e0d7b3845a8ed8c009d31dc2
Summy <2437404258@qq.com>
2018-06-22 11:37:51 +0000

    Add user statuses

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,15 +4,15 @@
             <a href="/" id="logo">Sample App</a>
             <nav>
                 <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::check())
+                    @if (\Illuminate\Support\Facades\Auth::check())
                         <li><a href="{{ route('users.index') }}">用户列表</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                {{ Auth::user()->name }} <b class="caret"></b>
+                                {{ \Illuminate\Support\Facades\Auth::user()->name }} <b class="caret"></b>
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a href="{{ route('users.show', Auth::user()->id) }}">个人中心</a></li>
-                                <li><a href="{{ route('users.edit', Auth::user()->id) }}">编辑资料</a></li>
+                                <li><a href="{{ route('users.show', \Illuminate\Support\Facades\Auth::user()->id) }}">个人中心</a></li>
+                                <li><a href="{{ route('users.edit', \Illuminate\Support\Facades\Auth::user()->id) }}">编辑资料</a></li>
                                 <li class="divider"></li>
                                 <li>
                                     <a id="logout" href="#">
